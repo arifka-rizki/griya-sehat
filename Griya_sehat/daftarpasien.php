f49676ad714afe19798ed33452da52c1e9c8890c
Ahmad Naghim <ahmadnaghim@gmail.com>
2019-11-26 12:20:32 +0700

    qweq

--- Griya_sehat/daftarpasien.php
+++ Griya_sehat/daftarpasien.php
@@ -118,7 +118,7 @@
         <div class="card mb-3">
           <div class="card-header">
             <i class="fas fa-table"></i>
-            Data Table Example</div>
+            Daftar Pasien</div>
           <div class="card-body">
             <div class="table-responsive">
               <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
@@ -137,7 +137,7 @@
               </table>
             </div>
           </div>
-          <div class="card-footer small text-muted">Updated yesterday at 11:59 PM</div>
+          <div class="card-footer small text-muted">Updated</div>
         </div>
 
         <p class="small text-center text-muted my-5">
--- Griya_sehat/daftarreservasi.php
+++ Griya_sehat/daftarreservasi.php
@@ -1,17 +1,53 @@
-    <!DOCTYPE html>
-    <html lang="en">
+<?php
+  session_start();
+  if($_SESSION["adminlog"]!==true){
+    header("location: index.html");
+  }
 
-    <head>
+  if($_SESSION["checkdate"]===true){
+      $curdate = $_SESSION["datex"];
+  }
+  else{
+      $curdate = date('Y-m-d');
+  }
 
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
+  $servername = "localhost";
+  $username = "root";
+  $password = "";
+  $dbname = "Griya_Sehat";
 
-    <title>Admin - Dashboard</title>
+  // Create connection
+  $conn = new mysqli($servername, $username, $password, $dbname);
+  // Check connection
+  if ($conn->connect_error) {
+      die("Connection failed: " . $conn->connect_error);
+  }
 
-    <!-- Custom fonts for this template-->
+  $sql = "SELECT * from reservasi where Tanggal='$curdate'";
+  $result = $conn->query($sql);
+  $row = $result->fetch_assoc();
+  
+  $dataresv = array($row);
+
+  while($row = $result->fetch_assoc()){
+    $newar = array_push($dataresv, $row);
+  }
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>SB Admin - Tables</title>
+
+  <!-- Custom fonts for this template-->
     <!--<link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">-->
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.6/css/all.css">
 
@@ -20,15 +56,15 @@
     <link rel="stylesheet" href="frontend/libraries/datatables/dataTables.bootstrap4.css">
     <!-- Custom styles for this template-->
     <!--<link href="css/sb-admin.css" rel="stylesheet">-->
-    <link rel="stylesheet" href="frontend/styles/sb-admin.css">
+  <link rel="stylesheet" href="frontend/styles/sb-admin.css">
 
-    </head>
+</head>
 
-    <body id="page-top">
+<body id="page-top">
 
     <nav class="navbar navbar-expand navbar-dark bg-dark static-top mr-auto">
 
-        <a class="navbar-brand mr-1" href="dashboardadmin.html".html">Admin Dashboard</a>
+        <a class="navbar-brand mr-1" href="dashboardadmin.php">Admin Dashboard</a>
 
         <button class="btn btn-link btn-sm text-white order-1 order-sm-0" id="sidebarToggle" href="#">
         <i class="fas fa-bars"></i>
@@ -51,15 +87,15 @@
         
         </ul>
 
-        </nav>
-        -->
+      </nav>
+      -->
         <ul class="navbar-nav  ml-auto ">
         
-            <li class="nav-item ">
-            <a class="nav-link change-color text-white font-weight-bold" href="adminlogin.html">Log Out</a>
-            </li>
+          <li class="nav-item ">
+            <a class="nav-link change-color text-white font-weight-bold" href="logout.php">Log Out</a>
+          </li>
         </ul>
-        </nav>
+      </nav>
     <div id="wrapper">
 
         <!-- Sidebar -->
@@ -71,7 +107,7 @@
             </a>
         </li>
         
-        <li class="nav-item">
+        <li class="nav-item ">
             <a class="nav-link" href="daftarpasien.php">
             <i class="fas fa-fw fa-table"></i>
             <span>Daftar Pasien</span></a>
@@ -86,83 +122,116 @@
 
         <div id="content-wrapper">
 
-        <div class="container-fluid">
-
-            <!-- Page Content -->
-            <h1>Blank Page</h1>
-            <hr>
-            <p>This is a great starting point for new custom pages.</p>
-
-        </div>
-        </div>
-        <!-- /.container-fluid -->
-
-        <!-- Sticky Footer -->
-        <footer class="sticky-footer">
-            <div class="container my-auto">
-            <div class="copyright text-center my-auto">
-                <span>Copyright © Your Website 2019</span>
+        <!-- DataTables Example -->
+        <div class="card mb-3">
+          <div class="card-header">
+            <i class="fas fa-table"></i>
+            Daftar Reservasi</div>
+          <div class="card-body">
+            <div class="table-responsive">
+            <form method="POST" action="changedate.php">
+                Tanggal
+            <input type="date" id="TanggalP" name="tanggal" value=<?php echo $curdate ?> required>
+            <button type="submit">Submit</button>
+            </form>
+            <?php if(count($dataresv)>1) : ?>
+            <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                <thead>
+                  <tr>
+                    <th><?php echo implode('</th><th>', array_keys(current($dataresv))); ?></th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <?php foreach ($dataresv as $rows): array_map('htmlentities', $rows); ?>
+                      <tr>
+                        <td><?php echo implode('</td><td>', $rows); ?></td>
+                      </tr>
+                  <?php endforeach; ?>
+                </tbody>
+              </table>
+            <?php else : ?>
+                Tidak ada reservasi
+            <?php endif ; ?>
             </div>
-            </div>
-        </footer>
-
+          </div>
+          <div class="card-footer small text-muted">Updated</div>
         </div>
-        <!-- /.content-wrapper -->
+
+        <p class="small text-center text-muted my-5">
+          <em>More table examples coming soon...</em>
+        </p>
+
+      </div>
+      <!-- /.container-fluid -->
+
+      <!-- Sticky Footer -->
+      <footer class="sticky-footer">
+        <div class="container my-auto">
+          <div class="copyright text-center my-auto">
+            <span>Copyright © Your Website 2019</span>
+          </div>
+        </div>
+      </footer>
 
     </div>
-    <!-- /#wrapper -->
+    <!-- /.content-wrapper -->
 
-    <!-- Scroll to Top Button-->
-    <a class="scroll-to-top rounded" href="#page-top">
-        <i class="fas fa-angle-up"></i>
-    </a>
+  </div>
+  <!-- /#wrapper -->
 
-    <!-- Logout Modal-->
-    <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-            <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
-            <button class="close" type="button" data-dismiss="modal" aria-label="Close">
-                <span aria-hidden="true">×</span>
-            </button>
-            </div>
-            <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
-            <div class="modal-footer">
-            <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
-            <a class="btn btn-primary" href="adminlogin.html">Logout</a>
-            </div>
+  <!-- Scroll to Top Button-->
+  <a class="scroll-to-top rounded" href="#page-top">
+    <i class="fas fa-angle-up"></i>
+  </a>
+
+  <<!-- Logout Modal--><!--
+  <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
+          <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">×</span>
+          </button>
         </div>
+        <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
+        <div class="modal-footer">
+          <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
+          <a class="btn btn-primary" href="adminlogin.html">Logout</a>
         </div>
+      </div>
     </div>
+  </div>
+-->
 
-    <!-- Bootstrap core JavaScript-->
+   <!-- Bootstrap core JavaScript-->
     <!-- <script src="vendor/jquery/jquery.min.js"></script>
     <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
     -->
-    <link rel="stylesheet" href="frontend/libraries/jquery/jquery-3.4.1.min.js">
-    <link rel="stylesheet" href="frontend/libraries/bootstrap/js/bootstrap.bundle.min.js">
+    <link rel="stylesheet" href="/frontend/libraries/jquery/jquery-3.4.1.min.js">
+    <link rel="stylesheet" href="/frontend/libraries/bootstrap/js/bootstrap.bundle.min.js">
 
     <!-- Core plugin JavaScript-->
     <!--<script src="vendor/jquery-easing/jquery.easing.min.js"></script>-->
-    <link rel="stylesheet" href="frontend/libraries/jquery-easing/jquery.easing.min.js">
+    <link rel="stylesheet" href="/frontend/libraries/jquery-easing/jquery.easing.min.js">
 
     <!-- Page level plugin JavaScript-->
     <!--<script src="vendor/chart.js/Chart.min.js"></script>
     <script src="vendor/datatables/jquery.dataTables.js"></script>
     <script src="vendor/datatables/dataTables.bootstrap4.js"></script>-->
-    <script src="frontend/libraries/datatables/jquery.dataTables.js"></script>
-    <script src="frontend/libraries/datatables/dataTables.bootstrap4.js"></script>
+    <script src="/frontend/libraries/datatables/jquery.dataTables.js"></script>
+    <script src="/frontend/libraries/datatables/dataTables.bootstrap4.js"></script>
     <script src=""></script>
 
     <!-- Custom scripts for all pages-->
     <!--<script src="js/sb-admin.min.js"></script>-->
-    <script src="frontend/scripts/sb-admin.min.js"></script>
+    <script src="/frontend/scripts/sb-admin.min.js"></script>
 
     <!-- Demo scripts for this page-->
     <script src="js/demo/datatables-demo.js"></script>
     <script src="js/demo/chart-area-demo.js"></script>
-    
-    </body>
+    <script src="/frontend/scripts/demo/datatables-demo.js"></script>
 
-    </html>
+</body>
+
+</html>
